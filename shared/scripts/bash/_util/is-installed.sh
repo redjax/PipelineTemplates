149d83d9ac2166ca73090c1fdb325435d8794ba4
redjax <5534031+redjax@users.noreply.github.com>
2026-05-16 01:41:56 -0400

    script: Simplify is-installed check

--- shared/scripts/bash/_util/is-installed.sh
+++ shared/scripts/bash/_util/is-installed.sh
@@ -1,24 +1,26 @@
 #!/usr/bin/env bash
-set -euo pipefail
+set -uo pipefail
 
-########################################
-# Utility script to check if a command #
-# is available in the shell.           #
-#                                      #
-# Usage: is-installed.sh <cmd>         #
-#                                      #
-# Returns 0 if available, 1 otherwise  #
-########################################
+##################################################################################
+# Utility script to check if a command is available in the shell.                #
+#                                                                                #
+# The script can be called directly, i.e.:                                       #
+#   bash pipelinetemplates/shared/scripts/bash/_util/is-installed.sh <some-cmd>  #
+#                                                                                #
+# Or by sourcing it and running later, i.e.:                                     #
+#   . pipelinetemplates/shared/script/bash/_util/is-installed.sh                 #
+#   is_installed curl || exit 1                                                  #
+#                                                                                #
+# Returns 0 if available, 1 otherwise                                            #
+##################################################################################
 
-CHECK_CMD="${1:-}"
+is_installed() {
+  local cmd="$1"
+  [[ -z "$cmd" ]] && return 1
+  command -v "$cmd" >/dev/null 2>&1
+}
 
-if [[ -z "$CHECK_CMD" ]]; then
-  echo "[ERROR] No command was given" >&2
-  exit 1
-fi
-
-if command -v "$CHECK_CMD" >/dev/null 2>&1; then
-  exit 0
-else
-  exit 1
+## If script is called directly, run is_installed with $1
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+  is_installed "$1"
 fi
