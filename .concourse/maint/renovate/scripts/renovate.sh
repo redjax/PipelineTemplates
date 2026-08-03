53c85d0e7241a1f86a69216b6ae29753bf22e77e
redjax <5534031+redjax@users.noreply.github.com>
2026-08-03 01:30:34 -0400

    ci(renovate): Update renovate script
    Use uppercase env vars

--- .concourse/maint/renovate/scripts/renovate.sh
+++ .concourse/maint/renovate/scripts/renovate.sh
@@ -1,23 +1,23 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-mode="${mode:-run}"
-target_repo="${target_repo:-}"
-log_level="${log_level:-info}"
-config_file="${config_file:-renovate.json}"
-require_config="${require_config:-optional}"
-autodiscover="${autodiscover:-false}"
-renovate_author_email="${renovate_author_email:-5534031+redjax@users.noreply.github.com}"
-base_branches="${base_branches:-}"
-use_base_branch_config="${use_base_branch_config:-false}"
+mode="${MODE:-run}"
+target_repo="${TARGET_REPO:-}"
+log_level="${LOG_LEVEL:-info}"
+config_file="${CONFIG_FILE:-renovate.json}"
+require_config="${REQUIRE_CONFIG:-optional}"
+autodiscover="${AUTODISCOVER:-false}"
+renovate_author_email="${RENOVATE_AUTHOR_EMAIL:-5534031+redjax@users.noreply.github.com}"
+base_branches="${BASE_BRANCHES:-}"
+use_base_branch_config="${USE_BASE_BRANCH_CONFIG:-false}"
 
-: "${renovate_token:?renovate_token is required}"
-: "${github_api_token:?github_api_token is required}"
+: "${RENOVATE_TOKEN:?RENOVATE_TOKEN is required}"
+: "${GITHUB_COM_TOKEN:?GITHUB_COM_TOKEN is required}"
 
 if [[ -f "app/${config_file}" ]]; then
   selected_config="app/${config_file}"
-elif [[ -f "app/pipelinetemplates/config/renovate/default.json" ]]; then
-  selected_config="app/pipelinetemplates/config/renovate/default.json"
+elif [[ -f "pipelinetemplates/.concourse/maint/renovate/config/default.json" ]]; then
+  selected_config="pipelinetemplates/.concourse/maint/renovate/config/default.json"
 else
   echo "[ERROR] No renovate config found at 'app/${config_file}' or default template config." >&2
   exit 1
@@ -47,8 +47,8 @@ export RENOVATE_REPOSITORIES="${target_repo}"
 export RENOVATE_DRY_RUN="${dry_run}"
 export RENOVATE_CONFIG_FILE="${selected_config}"
 export RENOVATE_GIT_AUTHOR="Renovate Bot <${renovate_author_email}>"
-export GITHUB_COM_TOKEN="${github_api_token}"
+export GITHUB_COM_TOKEN="${GITHUB_COM_TOKEN}"
 export RENOVATE_BASE_BRANCHES="${base_branches}"
 export RENOVATE_USE_BASE_BRANCH_CONFIG="${use_base_branch_config}"
 
-exec /opt/renovate/entrypoint.sh "${renovate_token}"
+exec /opt/renovate/entrypoint.sh "${RENOVATE_TOKEN}"
