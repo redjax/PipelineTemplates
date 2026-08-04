e9342cc4afb3e4e95e14aa2b51f79d085b4e6148
redjax <5534031+redjax@users.noreply.github.com>
2026-08-04 14:38:11 -0400

    script: Debug echo platform value

--- .concourse/maint/renovate/scripts/renovate.sh
+++ .concourse/maint/renovate/scripts/renovate.sh
@@ -57,6 +57,7 @@ export RENOVATE_BASE_BRANCHES="${base_branches}"
 export RENOVATE_USE_BASE_BRANCH_CONFIG="${use_base_branch_config}"
 
 echo "[INFO] CONFIG: ${selected_config}"
+echo "[INFO] PLATFORM: ${platform}"
 echo "[INFO] MODE: ${mode}"
 echo "[INFO] TARGET_REPO: ${target_repo}"
 
